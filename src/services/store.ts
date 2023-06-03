6481722ff9dbb15ea4e1e8541f475c9221ea44f9
Matin Rezaei <matinrco@gmail.com>
2023-06-03 18:59:02 +0330

    Silent next-redux-wrapper cuz I don't need it right now

--- src/services/store.ts
+++ src/services/store.ts
@@ -52,5 +52,5 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const wrapper = createWrapper<AppStore>(makeStore, {
-    debug: process.env.NODE_ENV === "development",
+    debug: false,
 });
